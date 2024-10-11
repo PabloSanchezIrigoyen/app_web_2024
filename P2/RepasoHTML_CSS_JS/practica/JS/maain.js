85105dbb8fa3a20b3c29bf3da14247cd93f27e5a
PabloSanchezIrigoyen <pabloomanuel13@gmail.com>
2024-10-11 11:38:56 -0600

    1ER COMMIT REPASO_HTML_CSS_JS
    1ER COMMIT REPASO_HTML_CSS_JS

---
+++ P2/RepasoHTML_CSS_JS/practica/JS/maain.js
@@ -0,0 +1,16 @@
+//funciones 
+
+//1.- funciones que no recibe rapametros y no regresan valor 
+function suma1()
+{
+    let n1=2
+    let n2=3
+    let suma=n1+n2
+}
+
+
+
+
+//2.-no recibe parametros y regresa valor 
+//3.- recibe parametros y no regresa valor 
+//4.- recibe parametros y regresa valor 
